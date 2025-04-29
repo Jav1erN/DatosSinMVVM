c7f7f890e1ec58da7922d0e0bb73840e42f28e44
Javier <javier.neira@tecsup.edu.pe>
2025-04-29 14:23:56 -0500

    Cambios

---
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.datossinmvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DatosSinMVVMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Reemplaza Greeting por ScreenUser
+                    ScreenUser() // Llama a ScreenUser en lugar de Greeting
+                }
+            }
+        }
+    }
+}
